c44a58cbe0a042a821abc031fcb86059a3c1ca70
ezi000 <ezio18@o2.pl>
2023-11-08 18:53:03 +0100

    KONIEC

---
+++ script.js
@@ -0,0 +1,32 @@
+const dni = document.getElementById("dni");
+const godziny = document.getElementById("godziny");
+const minuty = document.getElementById("minuty");
+const sekundy = document.getElementById("sekundy");
+let nowyRok = new Date("01-01-2024");
+
+function odliczanie() {
+  let data = new Date();
+
+  let roznicaDat = nowyRok - data;
+
+  let ileDni = Math.floor(roznicaDat / (1000 * 60 * 60 * 24));
+  let ileGodzin = Math.floor((roznicaDat / (1000 * 60 * 60)) % 24);
+  let ileMinut = Math.floor((roznicaDat / (1000 * 60)) % 60);
+  let ileSekund = Math.floor((roznicaDat / 1000) % 60);
+
+  if (ileSekund < 10) {
+    sekundy.innerHTML = `0${ileSekund}`;
+  } else sekundy.innerHTML = ileSekund;
+
+  if (ileMinut < 10) {
+    minuty.innerHTML = `0${ileMinut}`;
+  } else minuty.innerHTML = ileMinut;
+
+  if (ileGodzin > 0) {
+    godziny.innerHTML = `0${ileGodzin}`;
+  } else godziny.innerHTML = ileGodzin;
+
+  dni.innerHTML = ileDni;
+}
+odliczanie();
+setInterval(odliczanie, 1000);
